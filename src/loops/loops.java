ad919fb3171bb0c0b50b053ec6c14e594bc36c87
Saranyatest5 <saranyacse05@gmail.com>
2018-12-22 22:58:54 +0530

    My first commit

---
+++ src/loops/loops.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class loops 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=0;i<=10;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				/*System.out.println(i);*/
+				System.out.println(j);
+			}
+			
+		}
+		
+	}
+
+}
